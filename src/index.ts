6b4d97bb5f1f88e8eed6bc0c3eb61dc0ea29cf0b
Ryan Tam <ryan.tam@smarkets.com>
2018-08-12 22:06:25 +0100

    Fix existing lint issues

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
 import {
-  JupyterLab, JupyterLabPlugin
-} from '@jupyterlab/application';
+  JupyterLab, JupyterLabPlugin,
+} from "@jupyterlab/application";
 
-import '../style/index.css';
-
+import "../style/index.css";
 
 /**
  * Initialization data for the jupyterlab_code_formatter extension.
  */
 const extension: JupyterLabPlugin<void> = {
-  id: 'jupyterlab_code_formatter',
-  autoStart: true,
   activate: (app: JupyterLab) => {
-    console.log('JupyterLab extension jupyterlab_code_formatter is activated!');
-  }
+    // tslint:disable-next-line:no-console
+    console.log("JupyterLab extension jupyterlab_code_formatter is activated!");
+  },
+  autoStart: true,
+  id: "jupyterlab_code_formatter",
 };
 
 export default extension;
