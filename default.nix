3702ea133bedbc06146cc7109a673ed0b4d2dedd
Ryan Tam <ryantam626@gmail.com>
2019-12-01 01:28:22 +0000

    nix, Makefile, poetry: Use poetry provided by nix
    Turns out I am actually install poetry into the user's environment,
    which is bad.
    
    Due to the immaturity of any sort of poetry2nix package, I opted to a
    localisesd `.venv` managed by poetry provided by nix instead.

--- default.nix
+++ default.nix
@@ -4,9 +4,7 @@ stdenv.mkDerivation {
   name = "jupyterlab_code_formatter";
 
   buildInputs = [
-    python37Packages.pip
     python37Packages.poetry
-    python37Packages.virtualenv
     python37Packages.rpy2
     python37Packages.sphinx
     python37Packages.sphinx_rtd_theme
@@ -23,17 +21,9 @@ stdenv.mkDerivation {
     export NIX_ENV="jupyterlab_code_formatter"  # Purely for my zsh prompt to work
 
     SOURCE_DATE_EPOCH=$(date +%s)  # so that we can use python wheels
-    YELLOW='\033[1;33m'
-    NC="$(printf '\033[0m')"
-
-    if [ ! -d venv ]; then
-      echo -e "''${YELLOW}Creating python environment...''${NC}"
-      virtualenv venv
-    fi
-    export PATH=$PWD/venv/bin:$PATH
-    echo -e "''${YELLOW}Installing poetry...''${NC}"
-    curl -sSL https://raw.githubusercontent.com/sdispater/poetry/master/get-poetry.py | python
-
+    poetry config settings.virtualenvs.in-project true
+    export SERVEREXTENSION_PATH=$PWD/serverextension
+    export PATH=$SERVEREXTENSION_PATH/.venv/bin:$PATH
     export LABEXTENSION_PATH=$PWD/labextension
     export PATH=$LABEXTENSION_PATH/node_modules/.bin/:$LABEXTENSION_TESTS_PATH/node_modules/.bin/:$PATH
   '';
