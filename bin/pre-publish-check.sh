3bf294ebea41fc2f4cd6989b908f32f94d8b6d01
Ryan Tam <ryantam626@gmail.com>
2019-12-01 17:50:51 +0000

    bin: Check package-lock.json version too

--- bin/pre-publish-check.sh
+++ bin/pre-publish-check.sh
@@ -2,10 +2,11 @@
 
 SERVEREXTENSION_VERSION=$(grep "^version" -- ${SERVEREXTENSION_PATH}/pyproject.toml | awk -F'[" ]' '{print $4}')
 LABEXTENSION_VERSION=$(cat ${LABEXTENSION_PATH}/package.json | jq -r '.version')
+LABEXTENSION_LOCKED_VERSION=$(cat ${LABEXTENSION_PATH}/package-lock.json | jq -r '.version')
 CHANGELOG_VERSION=$(awk 'match($0, /(.*) [0-9]{4}-[0-9]{2}-[0-9]{2}/, a) {print a[1]}' docs/changelog.rst | head -n 1)
 
 
-if [ "${SERVEREXTENSION_VERSION}" = "${LABEXTENSION_VERSION}" ] && [ "${LABEXTENSION_VERSION}" = "${CHANGELOG_VERSION}" ];
+if [ "${SERVEREXTENSION_VERSION}" = "${LABEXTENSION_VERSION}" ] && [ "${LABEXTENSION_VERSION}" = "${CHANGELOG_VERSION}" ] && [ "${LABEXTENSION_VERSION}" = "${LABEXTENSION_LOCKED_VERSION}" ];
 then
    echo "Versions seem fine."
 else
