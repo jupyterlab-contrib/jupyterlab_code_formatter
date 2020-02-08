64f94394aa9271de44317da52a9fafb3d4edbb9c
Ryan Tam <ryantam626@gmail.com>
2020-02-08 10:23:46 +0000

    bin: Fix pre publish check script
    Not really sure how it was working before with `sh`....
    
    Also update from pyproject.toml to setup.py, was missing from the switch
    before.

--- bin/pre-publish-check.sh
+++ bin/pre-publish-check.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-SERVEREXTENSION_VERSION=$(grep "^version" -- ${SERVEREXTENSION_PATH}/pyproject.toml | awk -F'[" ]' '{print $4}')
+SERVEREXTENSION_VERSION=$(grep "^version" -- ${SERVEREXTENSION_PATH}/setup.py | awk -F'"' '{ print $2 }')
 LABEXTENSION_VERSION=$(cat ${LABEXTENSION_PATH}/package.json | jq -r '.version')
 LABEXTENSION_LOCKED_VERSION=$(cat ${LABEXTENSION_PATH}/package-lock.json | jq -r '.version')
 CHANGELOG_VERSION=$(awk 'match($0, /(.*) [0-9]{4}-[0-9]{2}-[0-9]{2}/, a) {print a[1]}' docs/changelog.rst | head -n 1)
