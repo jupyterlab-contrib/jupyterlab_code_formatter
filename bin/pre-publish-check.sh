32f14296402fe61caffcaa330d8549ad2aa51c21
Ryan Tam <ryantam626@gmail.com>
2020-03-07 19:50:07 +0000

    bin: Fix pre publish check once again
    It's passing even on failure to parse the version correctly right now,
    fix that by checking the non-emptiness of the versions.
    
    Also fix the server extension version extraction command.

--- bin/pre-publish-check.sh
+++ bin/pre-publish-check.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-SERVEREXTENSION_VERSION=$(grep "^version" -- ${SERVEREXTENSION_PATH}/setup.py | awk -F'"' '{ print $2 }')
+SERVEREXTENSION_VERSION=$(grep "version" -- ${SERVEREXTENSION_PATH}/setup.py | awk -F'"' '{ print $2 }')
 LABEXTENSION_VERSION=$(cat ${LABEXTENSION_PATH}/package.json | jq -r '.version')
 LABEXTENSION_LOCKED_VERSION=$(cat ${LABEXTENSION_PATH}/package-lock.json | jq -r '.version')
 CHANGELOG_VERSION=$(awk 'match($0, /(.*) [0-9]{4}-[0-9]{2}-[0-9]{2}/, a) {print a[1]}' docs/changelog.rst | head -n 1)
@@ -8,7 +8,11 @@ HARDCODED_LABEXTENSION_CLIENT_VERSION=$(grep "PLUGIN_VERSION" -- ${LABEXTENSION_
 
 versions=( $SERVEREXTENSION_VERSION $LABEXTENSION_VERSION $LABEXTENSION_LOCKED_VERSION $CHANGELOG_VERSION $HARDCODED_LABEXTENSION_CLIENT_VERSION )
 unique_versions_count=$(echo ${versions[@]} | tr ' ' '\n' | uniq | wc -l)
-if [ "${unique_versions_count}" = 1 ]
+if [ "${unique_versions_count}" = 1 ] \
+  && [ ! -z "${LABEXTENSION_LOCKED_VERSION}" ] \
+  && [ ! -z "${LABEXTENSION_VERSION}" ] \
+  && [ ! -z "${CHANGELOG_VERSION}" ] \
+  && [ ! -z "${HARDCODED_LABEXTENSION_CLIENT_VERSION}" ]
 then
    echo "Versions seem fine."
    export PUBLISH_VERSION="v${CHANGELOG_VERSION}"
