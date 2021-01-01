e4e5a444fbb9ab0f50f06e27756c6f6b1f3b3b64
Ryan Tam <ryantam626@gmail.com>
2021-01-01 14:27:33 +0000

    Fix publishing script

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
+set -euxo pipefail
+
+TMP_DIR=npm-tmp
+
 source bin/pre-publish-check.sh
-cd $LABEXTENSION_PATH
-npm publish --access public
-cd $SERVEREXTENSION_PATH
+mkdir -p $TMP_DIR
+pushd $TMP_DIR
+npm pack ../
+npm publish ryantam626-jupyterlab_code_formatter-"${PUBLISH_VERSION/v/}".tgz --access public
+popd
 python setup.py sdist
 twine upload dist/jupyterlab_code_formatter-"${PUBLISH_VERSION/v/}".tar.gz
 git tag -a "${PUBLISH_VERSION}" -m "${PUBLISH_VERSION}"
