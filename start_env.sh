36f2de4163003abcb660cf997faa92c52abe4d3e
Ryan Tam <ryantam626@gmail.com>
2021-01-01 13:51:53 +0000

    Clean up start_env.sh

--- start_env.sh
+++ start_env.sh
@@ -1,5 +1,4 @@
 conda activate jupyterlab_code_formatter
 export SERVEREXTENSION_PATH=$PWD
-export PATH=$SERVEREXTENSION_PATH/.venv/bin:$PATH
 export LABEXTENSION_PATH=$PWD/src
-export PATH=$LABEXTENSION_PATH/node_modules/.bin/:$LABEXTENSION_TESTS_PATH/node_modules/.bin/:$PATH
+export PATH=$LABEXTENSION_PATH/node_modules/.bin/:$PATH
