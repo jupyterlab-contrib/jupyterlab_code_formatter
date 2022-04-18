37238b826bce11a52f95b4d798dd25f8da92e25d
Ryan Tam <ryantam626@gmail.com>
2022-04-18 21:23:51 +0100

    Modernise CI and fix pesky perm issues of docs

--- bin/build-docs.sh
+++ bin/build-docs.sh
@@ -2,18 +2,21 @@
 
 case "$1" in
   watch)
-    rm -rf docs/ &&
-    python3 -m sphinx -a -b html docs_src docs &&
-    touch docs/.nojekyll &&
+    rm -rf docs/
+    python3 -m sphinx -a -b html docs_src docs
+    touch docs/.nojekyll
+    chown -R 1000 docs/
     inotifywait -m docs -e modify docs |
     while read path action file; do
       python3 -m sphinx -W --keep-going -a -b html docs_src docs
       touch docs/.nojekyll
+      chown -R 1000 docs/
     done
     ;;
 
   *)
     python3 -m sphinx -W --keep-going -a -b html docs_src docs
     touch docs/.nojekyll
+    chown -R 1000 docs/
     ;;
 esac
--- dev/scripts/docker-run.sh
+++ dev/scripts/docker-run.sh
@@ -4,8 +4,10 @@ set -euxo pipefail
 
 BASE_DIR="$(git rev-parse --show-toplevel)"
 
-docker run --entrypoint bash -it \
+docker run --entrypoint bash \
+  -it \
   -p 8011:8011 \
   -v ${BASE_DIR}:/host \
   --rm \
-  jupyterlab-code-formatter-dev
+  jupyterlab-code-formatter-dev \
+  $@
