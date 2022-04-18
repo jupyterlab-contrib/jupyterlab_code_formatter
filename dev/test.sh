1c9c32276aced4ab734564059fbe10adb5924f82
Ryan Tam <ryantam626@gmail.com>
2022-04-18 22:13:53 +0100

    Fix CI browser check

--- dev/test.sh
+++ dev/test.sh
@@ -5,5 +5,5 @@ set -euxo pipefail
 pytest /host/jupyterlab_code_formatter
 jupyter server extension list 2>&1 | grep -ie "jupyterlab_code_formatter.*OK"
 jupyter labextension list 2>&1 | grep -ie "@ryantam626/jupyterlab_code_formatter.*OK"
-python -m jupyterlab.browser_check
+python -m jupyterlab.browser_check --allow-root
 
