906a14603f4def970280395b6c8da3c53c3b8281
Ryan Tam <ryantam626@gmail.com>
2022-05-01 12:40:27 +0100

    Use yarn for publishing, mount gitconfig so I can tag
    npm is swallowing my PATH, use yarn which doesn't do that, I cba to
    figure this out.

--- bin/publish.sh
+++ bin/publish.sh
@@ -6,10 +6,8 @@ TMP_DIR=npm-tmp
 
 source bin/pre-publish-check.sh
 mkdir -p $TMP_DIR
-pushd $TMP_DIR
-npm pack ../
-npm publish ryantam626-jupyterlab_code_formatter-"${PUBLISH_VERSION/v/}".tgz --access public
-popd
+yarn pack --filename $TMP_DIR/ryantam626-jupyterlab_code_formatter-"${PUBLISH_VERSION/v/}".tgz ../
+yarn publish $TMP_DIR/ryantam626-jupyterlab_code_formatter-"${PUBLISH_VERSION/v/}".tgz --access public
 python setup.py sdist
 twine upload dist/jupyterlab_code_formatter-"${PUBLISH_VERSION/v/}".tar.gz
 python setup.py bdist_wheel
