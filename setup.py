b9fc2027ac21be8721bf515a4305ff77bafb6343
Hao Xin <haoxinst@gmail.com>
2022-05-09 14:18:48 +0800

    chore: Add Python 3.10 to the supports list

--- setup.py
+++ setup.py
@@ -92,6 +92,7 @@ setup_args = dict(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.10",
         "Framework :: Jupyter",
     ],
 )
